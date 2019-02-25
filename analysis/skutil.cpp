ce4bea126c0df0407c7f29bf0f55258e02b7e052
Artur Sztuc <asztuc@sukap002v301.km.icrr.u-tokyo.ac.jp>
2019-02-25 22:42:59 +0900

    SK utils for towall written by Okamura/Okajima-san
    Calculates distance between vertices, towall etc, so pretty useful.
    Probably should be in a different folder.

---
+++ analysis/skutil.cpp
@@ -0,0 +1,198 @@
+// A helper util/script written by okumura-san.
+// It can easily calculate towall, distance between two vertices etc.
+// Author: Okumura-san
+#include "skutil.h"
+
+Double_t skutil::dist( TVector3 & x,  TVector3 & y ) {
+  TVector3 a = x - y ;
+  return a.Mag();
+}
+
+Double_t skutil::dist( float x[],  float y[] ) {
+  TVector3 a( x[0], x[1], x[2] )  ;
+  TVector3 b( y[0], y[1], y[2] )  ;
+  return dist( a, b ) ;
+}
+
+
+Double_t skutil::distx( TVector3 & x,  TVector3 & y, TVector3 & dir  ) {
+  TVector3 a = x - y ;
+  Double_t d = dir.Mag() ; 
+  return d>0. ? a.Dot( dir )/ d : 0. ;
+}
+
+Double_t skutil::distx( float x[],  float y[], float z[] ) {
+  TVector3 a( x[0], x[1], x[2] )  ;
+  TVector3 b( y[0], y[1], y[2] )  ;
+  TVector3 c( z[0], z[1], z[2] )  ;
+  return distx( a, b, c ) ;
+}
+
+
+Double_t skutil::distyz( TVector3 & x,  TVector3 & y, TVector3 & dir  ) {
+  TVector3 a = x - y ;
+  return a.Perp( dir ) ;
+}
+
+Double_t skutil::distyz( float x[],  float y[], float z[] ) {
+  TVector3 a( x[0], x[1], x[2] )  ;
+  TVector3 b( y[0], y[1], y[2] )  ;
+  TVector3 c( z[0], z[1], z[2] )  ;
+  return distyz( a, b, c ) ;
+}
+
+
+Double_t skutil::wallsk( TVector3 & pos ) {
+  float a[3] ; 
+  a[0] = pos.X();
+  a[1] = pos.Y();
+  a[2] = pos.Z();
+  return wallsk( a ) ;
+}
+
+Double_t skutil::wallsk( float pos[] ) {
+  double r = pos[0]*pos[0] + pos[1]*pos[1] ; 
+  r = TMath::Sqrt( r ) ;
+  double wall = skutil::RINTK - r ; 
+  double z1 = skutil::ZPINTK - pos[2] ;
+  double z2 = pos[2] - skutil::ZMINTK ;
+  if ( z1<wall ) wall = z1 ; 
+  if ( z2<wall ) wall = z2 ; 
+  return wall ; 
+}
+
+
+Double_t skutil::towall( TVector3 & pos ,  TVector3 & dir  ) {
+  float pos_[3], dir_[3] ; 
+  pos_[0] = pos.X() ;
+  pos_[1] = pos.Y() ;
+  pos_[2] = pos.Z() ;
+  dir_[0] = dir.X() ;
+  dir_[1] = dir.Y() ;
+  dir_[2] = dir.Z() ;
+  return towall( pos_, dir_ ) ;
+}
+
+Double_t skutil::towall( double pos[], double dir[] )
+{
+	float pos_[3]={}, dir_[3]={};
+	for( int i=0; i<3; i++ ){
+		pos_[i]=(float)pos[i];
+		dir_[i]=(float)dir[i];
+	}
+	return towall( pos_, dir_ );
+
+}
+
+
+Double_t skutil::towall( float pos[] ,  float dir[]  ) {
+  Double_t hpos[3], rcir, cz ;
+  Int_t hit = 0 ;
+  if ( dir[2] < 0.0 ) {
+    if ( pos[2] < ZMINTK ) {
+      hit = 0 ;
+    } else {
+      cz = ( ZMINTK - pos[2] ) / dir[2] ; 
+      hpos[0] = cz * dir[0] + pos[0] ; 
+      hpos[1] = cz * dir[1] + pos[1] ; 
+      hpos[2] = ZMINTK ; 
+      rcir = sqrt( hpos[0]*hpos[0] + hpos[1]*hpos[1] ) ; 
+      if ( rcir<RINTK ) hit = 3 ;         // hit on BOTTOM plane
+    }
+  } else if ( dir[2] > 0.0 ) {
+    if ( pos[2] > ZPINTK ) {
+      hit = 0 ;
+    } else {
+      cz = ( ZPINTK - pos[2] ) / dir[2] ; 
+      hpos[0] = cz * dir[0] + pos[0] ; 
+      hpos[1] = cz * dir[1] + pos[1] ; 
+      hpos[2] = ZPINTK ; 
+      rcir = sqrt( hpos[0]*hpos[0] + hpos[1]*hpos[1] ) ; 
+      if ( rcir<RINTK ) hit = 2 ;          // hit on TOP plane
+    }
+  }
+  if ( hit==0 ) {
+    Double_t a0 = dir[0]*dir[0]+dir[1]*dir[1] ; 
+    Double_t b0 = dir[0]*pos[0]+dir[1]*pos[1] ; 
+    Double_t c0 = pos[0]*pos[0]+pos[1]*pos[1]-RINTK*RINTK ; 
+    Double_t hanbe = b0*b0-c0*a0 ; 
+    if ( hanbe >= 0. ) {
+      Double_t cz1 = (-b0+sqrt(hanbe))/a0 ; 
+      if ( cz1 > 0. ) {
+	hpos[0] = cz1 * dir[0] + pos[0] ; 
+	hpos[1] = cz1 * dir[1] + pos[1] ; 
+	hpos[2] = cz1 * dir[2] + pos[2] ; 
+	if ( hpos[2] <= ZPINTK && hpos[2] >= ZMINTK ) {
+	  hit=1 ;
+	}
+      }
+    }
+  }
+  Double_t towall ; 
+  if ( hit==0 ) {
+    towall = 0 ;
+  } else {
+    //    std::cout << "*** " << hpos[0] << " " << hpos[1] << " " << hpos[2] << std::endl ; 
+    towall = sqrt( pow((pos[0]-hpos[0]),2.0) + pow((pos[1]-hpos[1]),2.0) + pow((pos[2]-hpos[2]),2.0) ) ; 
+/*
+        std::cout<< " *** (towall, hit) : (" << towall <<" , " << hit <<" ), (" <<
+                    pos[0] << ","<< pos[1] <<"," << pos[2] <<"), ("<<
+                    dir[0] << ","<< dir[1] <<"," << dir[2] <<") ***" <<std::endl;
+*/
+
+  }
+  return towall ;
+}
+
+Double_t skutil::fromwall( TVector3 & pos ,  TVector3 & dir ) {
+  TVector3 dir_( -dir.X(), -dir.Y(), -dir.Z() )   ;
+  return towall( pos, dir_ ) ;
+}
+
+Double_t skutil::fromwall( float pos[] ,  float dir[] ) {
+  float dir_[3] ; 
+  dir_[0] = -dir[0] ; 
+  dir_[1] = -dir[1] ; 
+  dir_[2] = -dir[2] ; 
+  return towall( pos, dir_ ) ;
+}
+
+//
+//  get upper edge in which ratio of events contains
+//
+Double_t skutil::fitres( TH1 *h, float ratio ) {
+
+  Int_t nbin = h->GetNbinsX() ;
+  Double_t sum = h->Integral( 1, nbin+1 ) ;  // include overflow
+  Double_t sum2 = h->Integral( 1, nbin ) ;  
+  Double_t target = sum * ratio ; 
+  Double_t x ; 
+  if ( target > sum2 ) { 
+    x = h->GetBinContent( nbin ) ; 
+  } else if ( target < 0. ) {
+    x = h->GetBinContent( 1 ) ; 
+  } else {
+    Int_t low=1, hi=nbin, mid ; 
+    Double_t a, b, c ; 
+    a = h->Integral( 1, low ) ; 
+    c = h->Integral( 1, hi ) ; 
+    while ( hi-low > 1 ) {
+      mid = (low+hi)/2 ;
+      b = h->Integral( 1, mid ) ; 
+      if ( b < target ) { 
+	low = mid  ;
+	a = b ; 
+      } else {
+	hi = mid  ;
+	c = b ; 
+      }
+    }
+    Double_t x1 = h->GetXaxis()->GetBinCenter( low ) ;
+    Double_t x2 = h->GetXaxis()->GetBinCenter( hi ) ;
+    x = x1 + (target-a)/(c-a)*(x2-x1) ;
+    //    cout << low << " " << hi << endl ;
+    //    cout << x1 << " " << x2 << endl ;
+    //    cout << a << " " << b << " " << c  << endl ;
+  }
+  return x ; 
+}
---
+++ analysis/skutil.h
@@ -0,0 +1,68 @@
+// A helper util/script written by okumura-san.
+// It can easily calculate towall, distance between two vertices etc.
+// Author: Okumura-san
+#ifndef SKUTIL_HH_
+#define SKUTIL_HH_
+
+#include <iostream> 
+#include <fstream> 
+#include <cmath> 
+
+#include "TROOT.h"
+#include "TVector3.h"
+#include "TH1.h"
+#include "TMath.h"
+
+
+namespace skutil {
+
+  //
+  // SK tank parameter from geotnk.h 
+  //
+  const Double_t DITKTK = 3930. ; 
+  const Double_t HITKTK=4140. ; 
+  const Double_t DIINTK=3380. ; 
+  const Double_t HIINTK=3620. ; 
+  const Double_t HIWAL=3605.7 ; 
+  const Double_t TTATTK=260. ; 
+  const Double_t TBATTK=260. ; 
+  const Double_t TWATTK=275. ; 
+  const Double_t RTKTK = DITKTK/2. ; 
+  const Double_t ZPTKTK=HITKTK/2. ; 
+  const Double_t ZMTKTK=-HITKTK/2. ; 
+  const Double_t RINTK=DIINTK/2. ; 
+  const Double_t ZPINTK=HIINTK/2. ; 
+  const Double_t ZMINTK=-HIINTK/2. ; 
+  const Double_t ZPWAL=HIWAL/2. ; 
+  const Double_t ZMWAL=-HIWAL/2. ; 
+  const Double_t ZCNTTK=HITKTK/2. ;
+  const Double_t RMED  = 55. ;
+  const Double_t ZMED  = 55. ;
+  
+
+  Double_t dist( TVector3 &,  TVector3 & ) ;
+  Double_t dist( float [],  float [] ) ;
+
+  Double_t distx( TVector3 &,  TVector3 & , TVector3 & ) ;
+  Double_t distx( float [],  float [], float [] ) ;
+
+  Double_t distyz( TVector3 &,  TVector3 & , TVector3 & ) ;
+  Double_t distyz( float [],  float [], float [] ) ;
+
+  Double_t wallsk( TVector3 & ) ;
+  Double_t wallsk( float [] ) ;
+
+  Double_t towall( TVector3 &,  TVector3 &  ) ;
+  Double_t towall( float [],  float [] ) ;
+  Double_t towall( double [],  double [] ) ;
+
+  Double_t fromwall( TVector3 &,  TVector3 & ) ;
+  Double_t fromwall( float [],  float [] ) ;
+
+  Double_t fitres( TH1 *,  float ) ;
+
+}
+
+
+#endif
+
