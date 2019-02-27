5b91a0c6722a266c703184c9490aa699c8e74147
Artur Sztuc <asztuc@sukap002v301.km.icrr.u-tokyo.ac.jp>
2019-02-27 19:20:58 +0900

    Adding more 1R branches and fqwall

--- fitqun/fqevent.h
+++ fitqun/fqevent.h
@@ -5,17 +5,27 @@ struct fqevent{
   // Certain SK parameters
   float wall; 
 
-  // fiTQun 1R parameters
-  float fq1rmom[150][6];
-  float fq1rnll[150][6];
+  // 
 
+  // Subevent information
   int fqnse;
+
+  // fiTQun 1R parameters
+  float fq1rmom[150][6];    // momentum
+  float fq1rnll[150][6];    // nll
+  float fq1rpos[150][7][3]; // v position
+  float fq1rdir[150][7][3]; // v direction
+  
 
   // fiTQun MR parameters
   int fqnmrfit;
   int fqmrnring[150];
   float fqmrnll[150];
   float fqmrpid[150][6];
+
+  // User defined/custom variables
+  float fqtowall[7]; // This will be towall for [0][7] fq1rpos/dir
+  float fqwall[7];
 
 };
 #endif
